465595c0da998d20f353698caa16f2ca6e66d868
EmaVend <emanuele.vendetti@gmail.com>
2021-09-20 09:42:24 +0200

    [AP-1] prova commit

--- FirstProjectUsingGit/src/packageOne/FirstProjectUsingGitMain.java
+++ FirstProjectUsingGit/src/packageOne/FirstProjectUsingGitMain.java
@@ -3,7 +3,7 @@ package packageOne;
 public class FirstProjectUsingGitMain {
 
 	public static void main(String[] args) {
-		System.out.println("back home");
+		System.out.println("try integration with jira");
 
 	}
 
